75d0e07a502581d4923750f50dffbf9d749d11e3
wdgWon <same666@naver.com>
2023-12-04 21:24:50 +0900

    [feat] : 영화 목록과 영화 세부항목 분리하여 스토어 작성

--- src/stores/movies.ts
+++ src/stores/movies.ts
@@ -1,43 +1,35 @@
-import { reactive, computed } from 'vue';
+import { ref, reactive, computed, watch, readonly } from 'vue';
 import { defineStore } from 'pinia';
+import type { Movies, MovieDetail } from './interface';
 
-interface ResponseValue {
-    Search: Search[];
-    totalResults: string;
-    Response: string;
+interface MoviesWithLoading extends Movies {
+    isLoading: boolean;
 }
-
-interface Search {
-    Title: string;
-    Year: string;
-    imdbID: string;
-    Type: string;
-    Poster: string;
-}
-
-interface initailValue extends ResponseValue {}
 
 export const useMovieStore = defineStore('movie', () => {
-    const movies = reactive({} as initailValue);
+    const movies = reactive({ isLoading: false } as MoviesWithLoading);
 
     const fetchMovies = async (search = '', page = 1) => {
         try {
+            movies.isLoading = true;
             const res = await fetch('/api/getMovies', {
                 method: 'POST',
                 body: JSON.stringify({ search, page }),
             });
-            const { Search, totalResults, Response }: ResponseValue = await res.json();
+            const { Search, totalResults, Response }: Movies = await res.json();
 
-            movies.totalResults = totalResults;
             movies.Response = Response;
 
             if (Response === 'True') {
-                Search.forEach((s) => (movies.Search = [...(movies.Search ?? []), s]));
+                movies.Search = Search.map(movie => ({...movie, Poster: movie.Poster.replace(/SX300.jpg$/, "SX200.jpg")}));
+                movies.totalResults = totalResults;
             } else {
                 alert('검색 결과가 없습니다!');
             }
         } catch (error) {
             console.error(error);
+        } finally {
+            movies.isLoading = false;
         }
     };
 
@@ -46,4 +38,48 @@ export const useMovieStore = defineStore('movie', () => {
     });
 
     return { movies, isMovies, fetchMovies };
+});
+
+export const useMovieDetailStore = defineStore('movie-detail', () => {
+    const movieDetail = reactive([] as MovieDetail[]);
+    const isLoading = ref(false);
+
+    const isMovieDetail = (movieId: string) => movieDetail.some(({ imdbID }) => imdbID === movieId);
+
+    const fetchMovieDetail = async (movieId: string, plot: 'short' | 'full' = 'short') => {
+        if (isMovieDetail(movieId)) return null;
+        try {
+            isLoading.value = true;
+            const res = await fetch('/api/getMovieDetail', {
+                method: 'POST',
+                body: JSON.stringify({ movieId, plot }),
+            });
+            const data: MovieDetail = await res.json();
+
+            if (data.Response === 'True') {
+                movieDetail.push({...data, Poster: data.Poster.replace(/SX300.jpg$/, "SX400.jpg")});
+            } else {
+                alert('검색 결과가 없습니다!');
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            isLoading.value = false;
+        }
+    };
+
+    watch(
+        () => movieDetail.length,
+        (len) => {
+            while (10 < len--) {
+                movieDetail.shift();
+            }
+        }
+    );
+
+    const getMovieDetail = (movieId: string) => {
+        return readonly(movieDetail.find(({ imdbID }) => imdbID === movieId) as MovieDetail);
+    };
+
+    return { movieDetail, isMovieDetail, isLoading, getMovieDetail, fetchMovieDetail };
 });

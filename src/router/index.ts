a29bfad773f42630545f627bcc92b9f9e572398d
wdgWon <same666@naver.com>
2023-12-04 21:26:28 +0900

    [feat] : 라우터 연동

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from "@/App.vue"
+import SearchViewVue from '@/views/SearchView.vue'
+import DetailViewVue from '@/views/DetailView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: App
+      name: 'search',
+      component: SearchViewVue
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue')
-    //   redirect: "/"
-    // }
+    {
+      path: '/detail/:movieId',
+      name: 'detail',
+      component: DetailViewVue
+    },
   ]
 })
 
